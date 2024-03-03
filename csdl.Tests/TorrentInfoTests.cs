3903078c54575e237a38198831938e28b08696d2
Albie Spriddell <albion@dragonfruit.network>
2024-03-03 18:34:35 +0000

    add test project

---
+++ csdl.Tests/TorrentInfoTests.cs
@@ -0,0 +1,34 @@
+using System.IO;
+using System.Threading.Tasks;
+
+namespace csdl.Tests;
+
+public class TorrentInfoTests
+{
+    [Theory]
+    [InlineData("big-buck-bunny.torrent", "Big Buck Bunny", 3)]
+    [InlineData("ubuntu-20.04.6-live-server-amd64.iso.torrent", "ubuntu-20.04.6-live-server-amd64.iso", 1)]
+    public async Task TestTorrentParsing(string fileName, string expectedName, int expectedFileCount)
+    {
+        var file = Path.GetFullPath(Path.Combine("files", fileName));
+        var torrentBytes = await File.ReadAllBytesAsync(file);
+        
+        var torrentFromFile = new TorrentInfo(file);
+        var torrentFromBytes = new TorrentInfo(torrentBytes);
+        
+        // name check
+        Assert.Equal(expectedName, torrentFromFile.Metadata.Name);
+        
+        // metadata check
+        Assert.Equal(torrentFromFile.Metadata, torrentFromBytes.Metadata);
+
+        // file count
+        Assert.Equal(expectedFileCount, torrentFromFile.Files.Count);
+        
+        // file equality
+        foreach (var torrentFile in torrentFromFile.Files)
+        {
+            Assert.Contains(torrentFile, torrentFromBytes.Files);
+        }
+    }
+}
