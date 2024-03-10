363216320556763405555769d74a1e42fb4b87dc
Albie Spriddell <albion@dragonfruit.network>
2024-03-10 19:19:15 +0000

    change state to use remapped values

--- csdl/Enums/TorrentState.cs
+++ csdl/Enums/TorrentState.cs
@@ -2,10 +2,12 @@ namespace csdl.Enums;
 
 public enum TorrentState
 {
-    CheckingFiles = 0,
-    DownloadingMetadata = 1,
-    Downloading = 2,
-    Finished = 3,
-    Seeding = 4,
-    CheckingResumeData = 7
+    Unknown = 0,
+    CheckingFiles = 1,
+    CheckingResume = 2,
+    DownloadingMetadata = 3,
+    Downloading = 4,
+    Seeding = 5,
+    Finished = 6,
+    Errored = 7
 }
