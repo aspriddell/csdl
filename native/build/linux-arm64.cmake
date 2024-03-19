ccbc397f1602cfd0e7a6093a8f13bf5f9e4efe57
Albie Spriddell <albion@dragonfruit.network>
2024-03-19 18:05:55 +0000

    use gcc over clang

--- native/build/linux-arm64.cmake
+++ native/build/linux-arm64.cmake
@@ -1,18 +1,12 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
-set(triple aarch64-linux-gnu)
+set(CMAKE_C_COMPILER /usr/bin/aarch64-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER /usr/bin/aarch64-linux-gnu-g++)
 
-set(CMAKE_C_COMPILER clang)
-set(CMAKE_C_COMPILER_TARGET ${triple})
-
-set(CMAKE_CXX_COMPILER clang++)
-set(CMAKE_CXX_COMPILER_TARGET ${triple})
-
-set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 
 set(VCPKG_TARGET_TRIPLET arm64-linux-release-dynamic)
 include($ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake)
--- native/build/linux-x64.cmake
+++ native/build/linux-x64.cmake
@@ -1,17 +1,12 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
 
-set(triple x86_64-linux-gnu)
+set(CMAKE_C_COMPILER /usr/bin/x86_64-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER /usr/bin/x86_64-linux-gnu-g++)
 
-set(CMAKE_C_COMPILER clang)
-set(CMAKE_C_COMPILER_TARGET ${triple})
-set(CMAKE_CXX_COMPILER clang++)
-set(CMAKE_CXX_COMPILER_TARGET ${triple})
-
-set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 
 set(VCPKG_TARGET_TRIPLET x64-linux-release-dynamic)
 include($ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake)
