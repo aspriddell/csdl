826a6ba161e8fd5be582d457b029d7dbaa8325f5
Albie Spriddell <albion@dragonfruit.network>
2024-03-19 16:56:42 +0000

    set libc to v6

--- native/build/linux-arm64.cmake
+++ native/build/linux-arm64.cmake
@@ -3,18 +3,17 @@ set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
 set(triple aarch64-linux-gnu)
 
-set(CMAKE_C_COMPILER /usr/bin/clang-12)
-set(CMAKE_CXX_COMPILER /usr/bin/clang++-12)
-
-set(CMAKE_SYSROOT /usr/aarch64-linux-gnu)
-
+set(CMAKE_C_COMPILER clang)
 set(CMAKE_C_COMPILER_TARGET ${triple})
+
+set(CMAKE_CXX_COMPILER clang++)
 set(CMAKE_CXX_COMPILER_TARGET ${triple})
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_SYSROOT /usr/aarch64-linux-gnu)
 
 set(VCPKG_TARGET_TRIPLET arm64-linux-release-dynamic)
 include($ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake)
