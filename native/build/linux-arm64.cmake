f32c200f0a1f48a751b7b7d9cb64ee5e4c557487
Albie Spriddell <albion@dragonfruit.network>
2024-03-19 13:14:49 +0000

    add x64 clang toolchain files

---
+++ native/build/linux-arm64.cmake
@@ -0,0 +1,17 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR aarch64)
+
+set(triple aarch64-linux-gnu)
+
+set(CMAKE_C_COMPILER clang)
+set(CMAKE_C_COMPILER_TARGET ${triple})
+set(CMAKE_CXX_COMPILER clang++)
+set(CMAKE_CXX_COMPILER_TARGET ${triple})
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+set(VCPKG_TARGET_TRIPLET arm64-linux-release-dynamic)
+include($ENV{VCPKG_HOME}/scripts/buildsystems/vcpkg.cmake)
