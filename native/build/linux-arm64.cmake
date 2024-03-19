59f9287df75e206e02674c4c7b8cfff3cf42c1f7
Albie Spriddell <albion@dragonfruit.network>
2024-03-19 16:28:41 +0000

    more fixes

--- native/build/linux-arm64.cmake
+++ native/build/linux-arm64.cmake
@@ -1,14 +1,15 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
+set(triple aarch64-linux-gnu)
+
 set(CMAKE_C_COMPILER /usr/bin/clang-12)
 set(CMAKE_CXX_COMPILER /usr/bin/clang++-12)
 
-set(triple aarch64-linux-gnu)
+set(CMAKE_SYSROOT /usr/aarch64-linux-gnu)
+
 set(CMAKE_C_COMPILER_TARGET ${triple})
 set(CMAKE_CXX_COMPILER_TARGET ${triple})
-
-set(CMAKE_FIND_ROOT_PATH /usr/${triple})
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
