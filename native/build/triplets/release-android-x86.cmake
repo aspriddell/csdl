e888bd78a6f1e41d0e0f1f13f9e897fac3c1ab4c
Leandro <18240088+leandroconsiglio@users.noreply.github.com>
2024-04-24 14:28:12 -0300

    Add Android support

---
+++ native/build/triplets/release-android-x86.cmake
@@ -0,0 +1,10 @@
+set(VCPKG_TARGET_ARCHITECTURE x86)
+set(VCPKG_CMAKE_SYSTEM_NAME Android)
+set(VCPKG_CMAKE_CONFIGURE_OPTIONS -DANDROID_ABI=x86)
+set(VCPKG_MAKE_BUILD_TRIPLET "--host=i686-linux-android")
+
+set(VCPKG_BUILD_TYPE release)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
+
+#[[include("${CMAKE_CURRENT_LIST_DIR}/triplet-common.cmake")]]
---
+++ native/build/triplets/release-android-x86_64.cmake
@@ -0,0 +1,10 @@
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CMAKE_SYSTEM_NAME Android)
+set(VCPKG_CMAKE_CONFIGURE_OPTIONS -DANDROID_ABI=x86_64)
+set(VCPKG_MAKE_BUILD_TRIPLET "--host=x86_64-linux-android")
+
+set(VCPKG_BUILD_TYPE release)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
+
+#[[include("${CMAKE_CURRENT_LIST_DIR}/triplet-common.cmake")]]
