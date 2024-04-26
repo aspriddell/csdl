d9f3339d00d0ecfca7bcc969b7d201548679b65e
Leandro <leandroconsiglio@hotmail.com>
2024-04-26 15:21:02 -0300

    Fix android-arm triplet

--- native/build/triplets/release-android-arm.cmake
+++ native/build/triplets/release-android-arm.cmake
@@ -1,5 +1,5 @@
 set(VCPKG_TARGET_ARCHITECTURE arm)
-set(VCPKG_CMAKE_CONFIGURE_OPTIONS -DANDROID_ABI=armeabi-v7a)
-set(VCPKG_MAKE_BUILD_TRIPLET "--host=armv7a-linux-android")
+set(VCPKG_CMAKE_CONFIGURE_OPTIONS -DANDROID_ABI=armeabi-v7a -DANDROID_ARM_NEON=OFF)
+set(VCPKG_MAKE_BUILD_TRIPLET "--host=armv7a-linux-androideabi")
 
 include("${CMAKE_CURRENT_LIST_DIR}/triplet-common-android.cmake")
