a2907c02f17ff9a6c094b4dfcadf55b1819dd8c3
Leandro <leandroconsiglio@hotmail.com>
2024-04-26 15:17:47 -0300

    Setup-ndk action New package-android job Fix android-x64 runtime name Create triplet-common-android

--- native/build/triplets/release-android-arm64.cmake
+++ native/build/triplets/release-android-arm64.cmake
@@ -1,10 +1,5 @@
 set(VCPKG_TARGET_ARCHITECTURE arm64)
-set(VCPKG_CMAKE_SYSTEM_NAME Android)
 set(VCPKG_CMAKE_CONFIGURE_OPTIONS -DANDROID_ABI=arm64-v8a)
 set(VCPKG_MAKE_BUILD_TRIPLET "--host=aarch64-linux-android")
 
-set(VCPKG_BUILD_TYPE release)
-set(VCPKG_CRT_LINKAGE dynamic)
-set(VCPKG_LIBRARY_LINKAGE static)
-
-#[[include("${CMAKE_CURRENT_LIST_DIR}/triplet-common.cmake")]]
+include("${CMAKE_CURRENT_LIST_DIR}/triplet-common-android.cmake")
