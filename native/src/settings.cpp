9ff9c7d183df56b8ee307ee58a6455bd20a0949f
Albie <albion@dragonfruit.network>
2024-09-09 15:33:29 +0000

    fix enum mapping build-time errors

--- native/src/settings.cpp
+++ native/src/settings.cpp
@@ -7,9 +7,49 @@
 
 #include <magic_enum.hpp>
 
+#pragma region "enum mapping"
+
+// magic_enum customization for libtorrent settings_pack enums
+// required for some platforms to work as expected
+namespace magic_enum::customize {
+    template <>
+    struct enum_range<libtorrent::settings_pack::int_types> {
+        static constexpr int min = libtorrent::settings_pack::int_type_base;
+        static constexpr int max = libtorrent::settings_pack::max_int_setting_internal;
+    };
+
+    template <>
+    struct enum_range<libtorrent::settings_pack::string_types> {
+        static constexpr int min = libtorrent::settings_pack::string_type_base;
+        static constexpr int max = libtorrent::settings_pack::max_string_setting_internal;
+    };
+
+    template <>
+    struct enum_range<libtorrent::settings_pack::bool_types> {
+        static constexpr int min = libtorrent::settings_pack::bool_type_base;
+        static constexpr int max = libtorrent::settings_pack::max_bool_setting_internal;
+    };    
+}
+
 template <typename T>
 typename std::enable_if<std::is_enum<T>::value, bool>::type
-set_value(const char* key, std::function<bool(T)> setter);
+set_value(const char* key, std::function<bool(T)> setter)
+{
+    if (key == nullptr)
+    {
+        return false;
+    }
+
+    auto enum_key = magic_enum::enum_cast<T>(key, magic_enum::case_insensitive);
+    if (!enum_key.has_value())
+    {
+        return false;
+    }
+
+    return setter(enum_key.value());
+}
+
+#pragma endregion
 
 lt::settings_pack* create_settings_pack()
 {
@@ -66,23 +106,4 @@ bool set_int(lt::settings_pack* pack, const char* key, int value)
         pack->set_int(key, value);
         return true;
     });
-}
-
-// helper method for converting strings to enums and performing callbacks based on the result
-template <typename T>
-typename std::enable_if<std::is_enum<T>::value, bool>::type
-set_value(const char* key, std::function<bool(T)> setter)
-{
-    if (key == nullptr)
-    {
-        return false;
-    }
-
-    auto enum_key = magic_enum::enum_cast<T>(key, magic_enum::case_insensitive);
-    if (!enum_key.has_value())
-    {
-        return false;
-    }
-
-    return setter(enum_key.value());
 }
