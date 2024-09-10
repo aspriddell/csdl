0499ec8635494d418448896f33ce75515888e262
Albie <albion@dragonfruit.network>
2024-09-10 09:52:39 +0000

    refactor CSDLSTRUCT #define

--- native/include/struct_align.h
+++ native/include/struct_align.h
@@ -6,9 +6,9 @@
 #define CS_NATIVE_STRUCT_ALIGN_H
 
 #ifdef _MSC_VER
-#define CSDLSTRUCT __declspec(align(8))
+#define CSDL_STRUCT __declspec(align(8))
 #elif defined(__GNUC__) || defined(__clang__)
-#define CSDLSTRUCT __attribute__((aligned(8)))
+#define CSDL_STRUCT __attribute__((aligned(8)))
 #else
 #error "Unsupported compiler"
 #endif
