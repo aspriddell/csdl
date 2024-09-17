a1e98d23177755fead65cfbc63bffd016dc4c5a6
Albie <albion@dragonfruit.network>
2024-09-17 10:50:41 +0100

    Upgrade to gcc/g++ 12 on linux builds (#13)

--- native/build/linux-x64.cmake
+++ native/build/linux-x64.cmake
@@ -1,11 +1,2 @@
-set(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_SYSTEM_PROCESSOR x86_64)
-
-set(CMAKE_C_COMPILER x86_64-linux-gnu-gcc)
-set(CMAKE_CXX_COMPILER x86_64-linux-gnu-g++)
-
-set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-
+include("${CMAKE_CURRENT_LIST_DIR}/linux-x64-toolchain.cmake")
 include($ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake)
