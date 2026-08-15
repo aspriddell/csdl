cb635ba29dbfbbc1c53f2dc01f2fa28cc69ecbeb
Albie <albion@dragonfruit.network>
2026-08-15 14:44:28 +0000

    more fpic changes

--- native/build/triplets/triplet-common-android.cmake
+++ native/build/triplets/triplet-common-android.cmake
@@ -7,3 +7,6 @@ set(VCPKG_LIBRARY_LINKAGE static)
 # Add -fPIC flag for all platforms (required for linking static libs into shared libs)
 set(VCPKG_CXX_FLAGS "-fPIC")
 set(VCPKG_C_FLAGS "-fPIC")
+
+# Ensure CMake-based ports build static libs as PIC as well.
+list(APPEND VCPKG_CMAKE_CONFIGURE_OPTIONS "-DCMAKE_POSITION_INDEPENDENT_CODE=ON")
