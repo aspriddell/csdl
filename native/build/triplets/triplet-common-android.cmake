0ad7313678da0406b7eedc63ca01862f8c7172d3
Albie <albion@dragonfruit.network>
2026-08-15 14:29:43 +0000

    change vcpkg props

--- native/build/triplets/triplet-common-android.cmake
+++ native/build/triplets/triplet-common-android.cmake
@@ -5,5 +5,5 @@ set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 
 # Add -fPIC flag for all platforms (required for linking static libs into shared libs)
-string(APPEND VCPKG_CXX_FLAGS_RELEASE " -fPIC")
-string(APPEND VCPKG_C_FLAGS_RELEASE " -fPIC")
+set(VCPKG_CXX_FLAGS "-fPIC")
+set(VCPKG_C_FLAGS "-fPIC")
--- native/build/triplets/triplet-common.cmake
+++ native/build/triplets/triplet-common.cmake
@@ -3,8 +3,8 @@ set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 
 # Add -fPIC flag for all platforms (required for linking static libs into shared libs)
-string(APPEND VCPKG_CXX_FLAGS_RELEASE " -fPIC")
-string(APPEND VCPKG_C_FLAGS_RELEASE " -fPIC")
+set(VCPKG_CXX_FLAGS "-fPIC")
+set(VCPKG_C_FLAGS "-fPIC")
 
 # build libtorrent as a dynamic library, static link everything else
 if(PORT MATCHES "libtorrent")
