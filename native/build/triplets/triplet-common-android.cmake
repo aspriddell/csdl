38a2b5be0728fd37eeee5a26de028215b50e37f2
Albie <albion@dragonfruit.network>
2026-08-15 13:57:31 +0000

    enable fPIC

--- native/build/triplets/triplet-common-android.cmake
+++ native/build/triplets/triplet-common-android.cmake
@@ -3,3 +3,7 @@ set(VCPKG_CMAKE_SYSTEM_NAME Android)
 set(VCPKG_BUILD_TYPE release)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
+
+# Add -fPIC flag for all platforms (required for linking static libs into shared libs)
+string(APPEND VCPKG_CXX_FLAGS_RELEASE " -fPIC")
+string(APPEND VCPKG_C_FLAGS_RELEASE " -fPIC")
--- native/build/triplets/triplet-common.cmake
+++ native/build/triplets/triplet-common.cmake
@@ -2,6 +2,10 @@ set(VCPKG_BUILD_TYPE release)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 
+# Add -fPIC flag for all platforms (required for linking static libs into shared libs)
+string(APPEND VCPKG_CXX_FLAGS_RELEASE " -fPIC")
+string(APPEND VCPKG_C_FLAGS_RELEASE " -fPIC")
+
 # build libtorrent as a dynamic library, static link everything else
 if(PORT MATCHES "libtorrent")
     set(VCPKG_LIBRARY_LINKAGE dynamic)
